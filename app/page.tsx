06ac75bc7dd10364fea0287f01bba72af3268886
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2026-04-06 10:30:26 +0400

    speed test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Content from "./components/content/Content";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/page";
+
 export const navItems = [
   { name: "მთავარი", href: "/" },
   {
